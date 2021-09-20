2f115b42b5393d97d848572e0b67e3f9aff5c3da
Emanuel Lucas Mizael dos Santos <79867442+mizaelucas@users.noreply.github.com>
2021-09-20 12:10:14 -0300

    feat: built the header component. added the project svgs to the src folder

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import * as S from 'styles/header';
+import { ReactComponent as Calendar } from 'assets/calendar.svg';
+import { ReactComponent as Location } from 'assets/location.svg';
+import { ReactComponent as Search } from 'assets/search.svg';
+
+const Header = () => {
+    return (
+        <S.Header>
+            <S.Wrapper>
+                <S.Title>EXOTIC</S.Title>
+                <S.Subtitle>CARS</S.Subtitle>
+            </S.Wrapper>
+            <S.Wrapper colored>
+                <S.Wrapper marginRight="4rem">
+                    <Location />
+                    <S.Text>North Carolina, NC 90025</S.Text>
+                </S.Wrapper>
+                <S.Wrapper marginRight="1rem">
+                    <Calendar />
+                    <S.Text>11/03/2021</S.Text>
+                </S.Wrapper>
+                <S.Wrapper marginRight="1rem">
+                    <Calendar />
+                    <S.Text>12/12/2021</S.Text>
+                </S.Wrapper>
+                <div
+                    style={{
+                        backgroundColor: '#fff',
+                        padding: '.2rem .5rem',
+                        borderRadius: '5rem'
+                    }}
+                >
+                    <Search />
+                </div>
+            </S.Wrapper>
+            <S.Wrapper>
+                <S.Button>Sign up</S.Button>
+                <S.Button primary>Sign in</S.Button>
+            </S.Wrapper>
+        </S.Header>
+    );
+};
+
+export default Header;
